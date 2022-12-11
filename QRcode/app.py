2bdb2d531a05a19b5818d1aec390e9658652135c
Prabal Pathak <prabal01pathak@gmail.com>
2022-12-11 09:27:47 +0000

    reset weldsem signal if priter signal is false

--- QRcode/app.py
+++ QRcode/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 from app_main import Manager, generate_QR
-#print("dj")
-app = Flask(__name__)  
+
+# print("dj")
+app = Flask(__name__)
 
 WELDSEM_SIGNAL = False
 PRINTER_SIGNAL = False
@@ -9,19 +10,22 @@ PRINTER_SIGNAL = False
 
 manager = Manager()
 
-@app.route('/')  
-def upload():  
-    return render_template("index.html")  
- 
-@app.route('/send_signal', methods = ['POST'])  
-def get_signal():  
-    if request.method == 'POST': 
-        weldsem_signal = True if request.form.get('WS') == "on" else False
-        printer_signal = True if request.form.get('PS') == "on" else False
-        print(weldsem_signal, printer_signal)
-    manager.main(weldsem_signal,printer_signal)    
-    #return render_template("index.html", QR_Image = 'img_path')  
+
+@app.route("/")
+def upload():
     return render_template("index.html")
 
-if __name__ == '__main__':  
-    app.run(debug = True) 
+
+@app.route("/send_signal", methods=["POST"])
+def get_signal():
+    if request.method == "POST":
+        weldsem_signal = True if request.form.get("WS") == "on" else False
+        printer_signal = True if request.form.get("PS") == "on" else False
+        print(weldsem_signal, printer_signal)
+    manager.main(weldsem_signal, printer_signal)
+    # return render_template("index.html", QR_Image = 'img_path')
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- QRcode/app_main.py
+++ QRcode/app_main.py
@@ -2,50 +2,58 @@ import qrcode
 import os
 
 
+default_meter_code = "00000"
+default_mother_code = "rai000000"
+# BASE_PATH = r"D:\Resolute AI\QRcode\savedQR"
+BASE_PATH = ""
 
-default_meter_code="00000"
-default_mother_code="rai000000"
-meter_increament = 0
-printer_increament = 0
-BASE_PATH = r"D:\Resolute AI\QRcode\savedQR",
-# BASE_PATH = ""
 
 def generate_QR(code):
-	qr=qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_L,box_size=10,border=2,)
-	qr.add_data(str(code))
-	qr.make(fit=True)
-	img=qr.make_image(fill_color="white", back_color="black")
-	img.save(os.path.join(BASE_PATH,str(code)+".png"))
-	print("QR Generated")
-
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=2,
+    )
+    qr.add_data(str(code))
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="white", back_color="black")
+    img.save(os.path.join(BASE_PATH, str(code) + ".png"))
+    print("QR Generated")
 
 
 class Manager:
-	def __init__(self):
-		self.printer_increament = 0
-		self.meter_increament = 0
+    def __init__(self):
+        self.printer_increament = 0
+        self.meter_increament = 0
 
-	# def meter_code(mother_code,printer_signal):
-	def generate_code(self, mothercode: str, printer_signal: bool=True):
-		if printer_signal:
-			self.printer_increament += 1
-			meter_code=default_meter_code[:-len(str(self.printer_increament))]+str(self.printer_increament)
-			print("Printer_Signal & metercode increased!!!")
-			code=mothercode+meter_code
-			print(f"code:{code}")
-			generate_QR(code)
+    # def meter_code(mother_code,printer_signal):
+    def generate_code(self, mothercode: str, printer_signal: bool = True):
+        if printer_signal:
+            self.printer_increament += 1
+            meter_code = default_meter_code[: -len(str(self.printer_increament))] + str(
+                self.printer_increament
+            )
+            print("Printer_Signal & metercode increased!!!")
+            code = mothercode + meter_code
+            print(f"code:{code}")
+            generate_QR(code)
+
+    def main(self, weldsem_signal: bool = False, printer_signal: bool = True):
+        if not printer_signal:
+            self.meter_increament = 1
+            self.printer_increament = 1
+        elif weldsem_signal:
+            self.meter_increament += 1
+            print("Weldsem_Signal & mothercode increased!!!")
+
+        mother_code = default_mother_code[: -len(str(self.meter_increament))] + str(
+            self.meter_increament
+        )
+        self.generate_code(mother_code, printer_signal=printer_signal)
 
 
-	def main(self, weldsem_signal: bool=False,printer_signal: bool=True):
-		if weldsem_signal:
-			self.meter_increament += 1
-			print("Weldsem_Signal & mothercode increased!!!")
-
-		mother_code=default_mother_code[:-len(str(self.meter_increament))]+str(self.meter_increament)
-		self.generate_code(mother_code,printer_signal=printer_signal)
-
-#main()
-
+# main()
 
 
 # def meter_code(mother_code,printer_signal):
@@ -68,16 +76,14 @@ class Manager:
 # 			print("printer_signal=False; break loop!!!")
 
 
-
-
 # def main(weldsem_signal="None",printer_signal="None"):
 # 	default_mother_code="rai000000"
 # 	weldsem_signal="printer_signal"#"F"
 # 	increament=0
 
-# 	while weldsem_signal!="E": 
+# 	while weldsem_signal!="E":
 # 		#weldsem_signal=input("Enter weldsem_signal: ")
-		
+
 # 		if weldsem_signal=="on":#"T":
 # 			increament+=1
 # 			print("Weldsem_Signal & mothercode increased!!!")
@@ -85,4 +91,4 @@ class Manager:
 # 		mother_code=default_mother_code[:-len(str(increament))]+str(increament)
 # 		generate_code(mother_code,printer_signal="on")#"T")
 
-#main()
+# main()
